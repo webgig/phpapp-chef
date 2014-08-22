4b882f91e81d4010daee0352dd71cc66ea596d53
root <root@precise32.(none)>
2014-08-22 13:07:22 +0000

    My PHPAPP Cookbools

---
+++ phpapp/metadata.rb
@@ -0,0 +1,11 @@
+name             'phpapp'
+maintainer       'webgig'
+maintainer_email 'webgig.sagar@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures phpapp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apache2"
+depends "mysql"
+depends "php"
---
+++ phpapp/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: phpapp
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "apache2"
+include_recipe "mysql::client"
+include_recipe "mysql::server"
+
+apache_site "default" do
+  enable true
+end
